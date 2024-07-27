a8101928f0a1019e819a89392fbacf99ff45aab0
disha@03 <disha.singhh@gmail.com>
2024-07-27 13:31:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      const id = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+      setIntervalId(id);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const startAndPause = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTiming = (time) => {
+    const getSeconds = `0${time % 60}`.slice(-2);
+    const minutes = Math.floor(time / 60);
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <div className="timer">{formatTiming(time)}</div>
+      <div>
+        <button onClick={startAndPause}>
+          {isRunning ? "Pause" : "Start"}
+        </button>
+        <button onClick={stop}>Stop</button>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
